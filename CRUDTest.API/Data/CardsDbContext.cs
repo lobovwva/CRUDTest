4f9daec3d6c2c3dd8ab27d6019c41beef952c190
Anastasia <108718603+lobovwva@users.noreply.github.com>
2023-07-24 22:00:14 +0600

    Добавьте файлы проекта.

---
+++ CRUDTest.API/Data/CardsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CRUDTest.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDTest.API.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // Dbset
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
